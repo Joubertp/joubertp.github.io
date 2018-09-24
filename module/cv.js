50cea0a4e7d7a3ae42a9572461d659b37b6f03f0
Joubertp <33349654+Joubertp@users.noreply.github.com>
2018-09-24 20:44:51 +0400

    Add files via upload

--- module/cv.js
+++ module/cv.js
@@ -2,52 +2,55 @@ import {
     newHeader2,
     newHeader3,
     newHeader4,
-    newParagraph} from './core.js'
+    newParagraph
+} from './core.js'
 
 
 export const cv = (() => {
 
-    const creatBox = (contentList,mother) =>{
+    const creatBox = (contentList, mother) => {
         const box = document.createElement('div')
-        for(let i = 0; i <= contentList.length-1 ; i++){
+        for (let i = 0; i <= contentList.length - 1; i++) {
             const content = newTopicBox(contentList[i])
             box.appendChild(content)
-        }        
+        }
         mother.appendChild(box)
     }
 
 
-    const newTopicBox = (content) =>{
-        const box = document.createElement('div')        
+    const newTopicBox = (content) => {
+        const box = document.createElement('div')
         const headerBox = newHeader2(content.title)
         box.appendChild(headerBox)
-        for(let i = 0; i <= content.content.length-1 ; i++){
+        for (let i = 0; i <= content.content.length - 1; i++) {
             const subtopicBox = newSubtopicBox(content.content[i])
             box.appendChild(subtopicBox)
-        }       
+        }
         return box
     }
 
-    const newSubtopicBox = (content) =>{
-        const box = document.createElement('div')        
+    const newSubtopicBox = (content) => {
+        const box = document.createElement('div')
         const header = newHeader3(content.title)
         box.appendChild(header)
-        for(let i = 0; i <= content.content.length-1 ; i++){
+        for (let i = 0; i <= content.content.length - 1; i++) {
             const descBox = newDescBox(content.content[i])
             box.appendChild(descBox)
         }
         return box
     }
 
-    const newDescBox = (content) =>{
-        const box = document.createElement('div')        
+    const newDescBox = (content) => {
+        const box = document.createElement('div')
         const header = newHeader4(content.title)
         box.appendChild(header)
-        if(content.date){
-           const date = newParagraph(content.date)
-           box.appendChild(date)
+        if (content.date) {
+            const date = newParagraph(content.date)
+            box.appendChild(date)
         }
+
         const body = newParagraph(content.content)
+        body.innerHTML = body.innerHTML.replace(/\n\r?/g, '<br />');
         box.appendChild(body)
         box.appendChild(document.createElement('br'))
         return box
